6f07fdca6c4832185d494a4462111f50e5f009da
Nukerush <71840171+Nukerush@users.noreply.github.com>
2020-09-25 17:26:27 +0300

    Add files via upload

---
+++ Space speed 1/Space speed 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Space_speed_1 
+    
+
+{
+    class Program
+    {
+        
+         
+
+        static void Main(string[] args)
+
+        { double H, V;
+          const double M = 5.976E24;
+          const double G = 6.672E-11;
+          const double R = 6371000;
+            Console.WriteLine("Введите высоту: ");
+             H = Convert.ToDouble(Console.ReadLine());
+             V = Math.Round(Math.Sqrt(G * M / R + H),0);
+            
+            Console.WriteLine($"Скорость спутника: {V} ");
+            
+        }
+    }
+}
